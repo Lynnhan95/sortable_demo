2fec165b059b97cf20811ee9db5d39e636f3ce37
你的英文名Lynnhan95 <han536@purdue.edu>
2019-01-30 00:29:03 -0500

    4

--- get.js
+++ get.js
@@ -2,7 +2,7 @@
     submit.onclick = function(){
         if(updateItem){
             var list1 = localStorage.getItem('list1')
-            window.confirm("Your result is\n" + list1 + ",ready to leave?")
+            window.confirm("Your result is\n" + list1 + ",ready for the result?")
             if(list1 == '13|1|2|3|4|5|6|7|8|9|10|11|12'){
                 window.confirm('Wow you score 100 on the test!!!')
             }else{
